6d5c17406e6474936a3313a708bb42d10bc8fde0
Zenarch911 <zeno277927@gmail.com>
2026-06-27 16:55:19 +0200

    feat: animations au scroll avec IntersectionObserver

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import { initLayout } from "./layout.js";
+import { initScrollReveal } from "./scroll.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   const page = document.body.dataset.page;
   if (page) initLayout(page);
+  initScrollReveal();
 });
